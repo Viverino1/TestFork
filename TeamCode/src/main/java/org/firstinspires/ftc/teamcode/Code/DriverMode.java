dd03ea1ab34493c168d415de1b963bb6b06d277e
vivekmaddineni <vivek.maddineni@gmail.com>
2023-03-16 09:55:01 -0500

    Simplified DriverMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/DriverMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code/DriverMode.java
@@ -4,13 +4,10 @@ import org.firstinspires.ftc.teamcode.VergeTools.*;
 
 public class DriverMode {
     Motor motor;
-    Chassis chassis;
     public void Init(HardwareMap hardwareMap){
         motor = new Motor("motor", hardwareMap);
-        chassis = new Chassis("1", "2", "3", "4", hardwareMap);
     }
     public void Loop() throws InterruptedException {
         motor.move(1);
-        chassis.x.move(1);
     }
 }
